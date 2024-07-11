5d9caadbc9522b4dd31b6ee4cbb953ed2a8532ea
bentran1vn <tan182205@gmail.com>
2024-07-12 02:14:09 +0700

    User Page

---
+++ DataAccessObject/Users/UserDAO.cs
@@ -0,0 +1,56 @@
+﻿using BusinessObject;
+using BusinessObject.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataAccessObject.Users;
+
+public class UserDAO
+{
+    private static UserDAO instance = null;
+    private static readonly object instanceLock = new();
+    private UserDAO() { }
+    public static UserDAO Instance
+    {
+        get
+        {
+            lock (instanceLock)
+            {
+                instance ??= new UserDAO();
+                return instance;
+            }
+        }
+    }
+
+    public async Task<User> Login(string username, string password)
+    {
+        try
+        {
+            using (var context = new MilkTeaDeliveryDBContext())
+            {
+                var account = await context.Users.SingleOrDefaultAsync(a => a.email == username && a.password == password && a.delete_By == null);
+                return account;
+            };
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public IEnumerable<User> GetAll()
+    {
+        try
+        {
+            using (var context = new MilkTeaDeliveryDBContext())
+            {
+                var account = context.Users.Include(u => u.Roles).Where(x => x.roleID != 2).AsNoTracking().AsEnumerable();
+                return account.ToList();
+            };
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+}
---
+++ Repositories/Users/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessObject.Entities;
+
+namespace Repositories.Users;
+
+public interface IUserRepository
+{
+    public Task<User> Login(string username, string password);
+    
+    public IEnumerable<User> GetAll();
+}
